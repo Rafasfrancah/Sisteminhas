b6d503a9f9c66059b98475d6fcd58f83e5ef0085
Rafasfrancah <rafasfrancah@gmail.com>
2023-04-28 07:25:17 -0300

    first commit

---
+++ ContasDoMes/js/contasDoMes.js
@@ -0,0 +1,7 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+let resposta = ""
+let numContas = 0
+let valTotal = 0
